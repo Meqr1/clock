3c9ce56c9d5211cdf5fb8d67444416ccb63acaaa
Meqr1 <ayanmahajan41@gmail.com>
2024-12-15 13:27:31 +0530

    added Tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import Tabs from './components/Tabs'
 import Clock from './components/Clock'
 import "./index.css"
+import { useState } from 'react'
+import { DataContext } from './dataContext'
 
 function App() {
+  const [active, setActive] = useState<string>("Clock")
+
   return (
-    <div className='h-screen w-screen bg-black text-white overflow-hidden'>
-      <Tabs />
-      <div className='flex items-center justify-center h-full'>
-        <Clock />
+    <DataContext.Provider value={{active: {active, setActive}}}>
+      <div className='h-screen w-screen bg-black text-white overflow-hidden'>
+        <Tabs />
+        <div className='flex items-center justify-center h-full'>
+          <Clock />
+        </div>
       </div>
-    </div>
+    </DataContext.Provider>
   )
 }
 
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,5 +1,38 @@
+import { useContext, useEffect, useState } from "react"
+import { DataContext } from "../dataContext"
+
 const Tabs = () => {
-  return <p className="">Tabs</p>
+  return (
+    <div className="transition-all duration-200 flex justify-center gap-10 mt-4" id="Tabs">
+      <Tab onClick={() => {}} name="Clock" />
+      <Tab onClick={() => {}} name="Stopwatch" />
+      <Tab onClick={() => {}} name="Timer" />
+    </div>
+  )
+}
+
+const Tab = (props: { onClick: () => void, name: string}) => {
+  const [active, setActive] = useState<boolean>(false)
+  const data = useContext(DataContext)
+
+  useEffect(() => {
+    if (data?.active.active === props.name) {
+      setActive(true)
+    } else {
+      setActive(false)
+    }
+  }, [data?.active.active, props.name])
+
+  const onClick = () => {
+    data?.active.setActive(props.name)
+  }
+
+  return (
+    <button id={`tab-${props.name}`} className="flex flex-col items-center justify-center" style={{"flexDirection": "column"}} onClick={onClick}>
+      {props.name}
+      <span className={`bg-red-600 h-[3px] w-10 block rounded-sm ${!active && "h-0 w-0"} transition-all`}></span>
+    </button>
+  )
 }
 
 export default Tabs
